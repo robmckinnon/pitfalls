d1b7b03fd76895a572f49f8d3377c8dc5a91c9c1
robmckinnon <rob@movingflow>
2020-10-28 06:05:10 +0700

    Fix grid library loading

--- lib/g.lua
+++ lib/g.lua
@@ -1,32 +1,32 @@
 local grid_lib = parameters.grid_lib()
-fn.dprint("grid_lib", grid_lib)
+local g = nil
+
 if grid_lib ~= "grid" then
+  print("grid_lib", grid_lib)
   local grid = include(grid_lib)
+  g = grid.connect()
+else
+  g = grid.connect()
 end
-local g = grid.connect()
 
 function g.init()
+  print("g.init", g)
+  local grid_h = g.rows
+  local grid_w = g.cols
   g.gfreqs = {}
   g.xleds = {}
   g.aleds = {}
   g.freqxy = {}
-  for x = 1,16 do
+  for x = 1,grid_w do
     g.xleds[x] = {}
     g.aleds[x] = {}
   end
-  for y = 1,8 do
-    for x = 1,16 do
-      g:led(x, y, 1)
-    end
-  end
-  g:refresh()
 end
   
 function g.key(x, y, z)
   local grid_h = g.rows
   local grid_w = g.cols
-  local f = g.gfreqs[8 - y + 1][x]
-
+  local f = g.gfreqs[grid_h - y + 1][x]
   if z > 0 then
     if g.freqxy[f]~=nil then
       for i,xy in pairs(g.freqxy[f]) do
@@ -45,34 +45,32 @@ function g.key(x, y, z)
 end
 
 function g.update_grid(scale, intervals, pitches)
+  local grid_h = g.rows
+  local grid_w = g.cols
   local offset = intervals:nearest_degree_to(3/2, nil) - 1
   local index = 0
   local degree = nil
   local freqsxy = {}
 
-  for row = 1,8 do
+  for row = 1,grid_h do
     g.gfreqs[row] = {}
     index = 1 + ((row - 1) * offset)
-    for col = 1,16 do
+    for col = 1,grid_w do
       degree = pitches:degree(index)
       fn.dprint("---", col)
       fn.dprint("", degree)
       fn.dprint("degree", degree)
       x = col
-      y = 8 - row + 1
+      y = grid_h - row + 1
       f = pitches:freq(index)
       if f == nil then
         print("f nil")
         print("index "..index)
       else
-        fn.dprint("f", f)
         g.gfreqs[row][col] = f
-        if pcall(freqsxy, f) then
-          -- we're good
-        else
+        if freqsxy[f] == nil then
           freqsxy[f] = {}
         end
-          
         freqsxy[f][#(freqsxy[f])+1] = {x, y}
       end
       
@@ -99,4 +97,3 @@ function g.update_grid(scale, intervals, pitches)
 end
 
 return g
-
