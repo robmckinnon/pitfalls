6dcf1dceb84b03b7e8c54aa593ef1cca478509a9
robmckinnon <rob@movingflow>
2020-12-10 23:15:09 +0000

    Make low/high pitch strings long/short in UI

--- lib/display_strings.lua
+++ lib/display_strings.lua
@@ -34,8 +34,9 @@ function display.redraw(base_freq, edit, octave, position, scale, intervals, mid
   s.update()
 end
 
-local INTERVAL_WIDTH = 88
-local INTEVAL_HEIGHT = 4
+local INTERVAL_WIDTH = 80
+local MAX_H = 80
+local INTERVAL_HEIGHT = 4
 local STEP_WIDTH = 7
 local STEP_MARGIN = 1
 
@@ -54,19 +55,37 @@ function display.drawsteps(edit, position, scale)
 end
 
 function display.drawintervals(scale, intervals)
-  local err, x, y
+  if scale.length > 9 then
+    INTERVAL_HEIGHT = 4
+  elseif scale.length > 7 then
+    INTERVAL_HEIGHT = 5
+  elseif scale.length > 5 then
+    INTERVAL_HEIGHT = 6
+  else
+    INTERVAL_HEIGHT = 7
+  end
+  local err, x, y, lab
+  x = INTERVAL_WIDTH + 5
+  y = INTERVAL_HEIGHT + 14
+  pf.line_rel(1, 0,MAX_H-y,     util.clamp(x-1,0,x),0)
+
   for i = 2,scale.length do
-    x = math.floor(intervals:ratio(i) * INTERVAL_WIDTH) - INTERVAL_WIDTH
-    y = (i*INTEVAL_HEIGHT) + 14
-    pf.line_rel(1, 0,y,     util.clamp(x-1,0,x),0)
+    -- x = math.floor(intervals:ratio(i) * INTERVAL_WIDTH) - INTERVAL_WIDTH
+
+    x = math.floor((1 / intervals:ratio(i)) * INTERVAL_WIDTH * 2) - INTERVAL_WIDTH + 5
+    y = (i*INTERVAL_HEIGHT) + 14
+    err = intervals:interval_error(i)
+    lab = intervals:uniq_interval_label(i)
+    pf.line_rel(lab == "" and 1 or pf.level_int(err),
+      0,MAX_H-y,
+      util.clamp(x-1, 0, x),0)
     -- pf.line_rel(5, x+28,y-1, 0,3)
 
-    err = intervals:interval_error(i)
     if (err ~= nil) then
       pf.text(
          pf.level_int(err),
-         x, y+2,
-         intervals:uniq_interval_label(i)
+         x, MAX_H-y+2,
+         lab
       )
     end
   end
@@ -80,63 +99,34 @@ local MAR3 = MAR2+7
 
 function display.drawLs(edit, scale)
   display_orig.drawLs(edit, scale)
-  -- pf.itext(L_INPUT, edit, scale,
-  --   MAR0,9,
-  --   "L: "..scale.large)
-
-  -- pf.itext(S_INPUT, edit, scale,
-  --   MAR0,17,
-  --   "s: "..scale.small)
 end
 
 function display.drawmidi(edit, scale, midi_start)
   display_orig.drawmidi(edit, scale, midi_start)
-  -- pf.itext(B_INPUT, edit, scale,
-  --   MAR0,25,
-  --   "b: "..pf.note_label(midi_start))
 end
 
 function display.drawscalesize(edit, scale)
   display_orig.drawscalesize(edit, scale)
-  -- pf.itext(N_INPUT, edit, scale,
-  --   MAR0,33,
-  --   "n: "..scale.length)
 end
--- 29
 
 function display.drawtuning(tuning, edit, scale)
   display_orig.drawtuning(tuning, edit, scale)
-  -- pf.itext(F_INPUT, edit, scale,
-  --   MAR3,9,
-  --   string.format("%.0f",tuning).."Hz")
 end
 
 function display.drawtonic(edit, scale)
   display_orig.drawtonic(edit, scale)
-  -- pf.itext(T_INPUT, edit, scale,
-  --   MAR3,17,
-  --   "t: "..scale.tonic)
 end
 
 function display.drawmode(edit, scale)
   display_orig.drawmode(edit, scale)
-  -- pf.itext(M_INPUT, edit, scale,
-  --   MAR3,25,
-  --   "m: "..scale.mode)
 end
 
 function display.drawoctave(edit, octave, scale)
   display_orig.drawoctave(edit, octave, scale)
-  -- pf.itext(O_INPUT, edit, scale,
-  --   MAR3,33,
-  --   "o: "..octave)
 end
 
 function display.drawedo(scale)
   display_orig.drawedo(scale)
-  -- pf.text(2,
-  --   90,55,
-  --   scale.edivisions.." EDO")
 end
 
 function display.edit_position(edit, scale)
