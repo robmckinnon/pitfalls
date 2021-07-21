ad2f44266905e25e88eb82250eac27a20182ff79
robmckinnon <rob@movingflow>
2021-07-22 02:51:14 +0700

    Initial code to use mx.samples

--- lib/includes.lua
+++ lib/includes.lua
@@ -1,11 +1,37 @@
--- ships with norns
-engine.name = "PolyPerc"
-
 -- represents scale with sequence of L,s steps
 include("pitfalls/lib/Scale")
 
 -- various functions
 pf = include("pitfalls/lib/functions")
+
+mx_samples = {}
+mxsamples=nil
+mx_sample=nil
+skeys=nil
+
+for f in io.popen("ls -a /home/we/dust/audio"):lines() do
+  if f == "mx.samples" then
+    print("mx.samples")
+    local pdir = io.popen("ls -a /home/we/dust/audio/mx.samples")
+    for s in pdir:lines() do
+      if s ~= "." and s ~= ".." then
+        print(s)
+        table.insert(mx_samples, s)
+      end
+    end
+    pdir:close()
+  end
+end
+
+if pf.tablelength(mx_samples) > 0 then
+  mxsamples=include("mx.samples/lib/mx.samples")
+  engine.name="MxSamples"
+  mx_sample=mx_samples[1]
+  skeys = mxsamples:new()
+else
+  -- ships with norns
+  engine.name = "PolyPerc"
+end
 
 -- maps ratio labels to ratio fractions
 ratiointervals = include("pitfalls/lib/ratios")
