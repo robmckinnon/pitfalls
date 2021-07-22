c9b60cd1b2a8d15d398118fc38cf412210994202
robmckinnon <rob@movingflow>
2021-07-23 05:30:23 +0700

    Show engine and patch name

--- lib/display_patch.lua
+++ lib/display_patch.lua
@@ -3,8 +3,8 @@ local s = screen
 
 function display.redraw(base_freq, edit, octave, position, scale, intervals, midi_start)
   s.clear()
-  -- display.drawsteps(edit, position, scale)
-  -- display.drawintervals(scale, intervals)
+  display_orig.drawsteps(edit, position, scale)
+  display.drawpatch(edit)
   display_orig.drawname(edit, scale, 5)
   display_orig.drawLs(edit, scale)
   display_orig.drawmidi(edit, scale, midi_start)
@@ -15,6 +15,13 @@ function display.redraw(base_freq, edit, octave, position, scale, intervals, mid
   display_orig.drawmode(edit, scale)
   display_orig.drawoctave(edit, octave, scale)
   s.update()
+end
+
+function display.drawpatch(edit)
+  pf.text(2,0,25,engine.name)
+  if (engine.name == "MxSamples") then
+    pf.text(2,0,36,mx_sample)
+  end
 end
 
 function display.edit_position(edit, scale)
