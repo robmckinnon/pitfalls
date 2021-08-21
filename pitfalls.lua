a4b92e53b96d87a991788607be517910915a49a3
robmckinnon <rob@movingflow>
2021-08-21 23:40:32 +0700

    Move arpeggio functions to separate file

--- pitfalls.lua
+++ pitfalls.lua
@@ -51,7 +51,6 @@ include("pitfalls/lib/includes")
 pf.debug(false)
 
 local run = false
-local position = 1
 
 local scale = nil
 local intervals = nil
@@ -61,10 +60,8 @@ local edit = 1
 
 function metrofast() counter.time = 0.25 end
 function metroslow() counter.time = 0.5 end
-function positionrand() position = math.random(scale.length) end
 
 local change = {}
-local arpeggiate = {}
 local display = display_circle
 
 function init()
@@ -115,6 +112,7 @@ function init()
   -- pf.pop_named_sequences(named_scales.lookup)
   display.drawintervals(scale, intervals)
 
+  arpeggiate.init(pitches_off, pitch_on_position)
   counter = metro.init(count, 0.125, -1)
   if run then
     counter:start()
@@ -122,12 +120,12 @@ function init()
 end
 
 function count()
-  arpeggiate[parameters.arpeggiate()]()
+  arpeggiate[parameters.arpeggiate()](scale, intervals)
   redraw()
 end
 
 function redraw()
-  display.redraw(params:get("tuning"), edit, params:get("octave"), position, scale, intervals, params:get("midi_start"), display == display_patch)
+  display.redraw(params:get("tuning"), edit, params:get("octave"), arpeggiate.position(), scale, intervals, params:get("midi_start"), display == display_patch)
 end
 
 function key(n,z)
@@ -173,7 +171,7 @@ end
 function update_pitches(update_intervals)
   if update_intervals then
     intervals = ScaleIntervals:new(scale)
-    chord_positions = {}
+    arpeggiate.reset_chord_positions()
   end
   pitches = Pitches:new(scale, intervals, params:get("tuning"), params:get("midi_start"))
   patch.note_off_all()
@@ -312,16 +310,14 @@ function update_arpeggiate(x)
     patch.note_off_all()
     patch.note_kill_all()
   elseif parameters.arpeggiate() == "scale_up" then
-    position = scale.length
+    arpeggiate.set_position(scale.length)
     run = true
     counter:start()
   elseif parameters.arpeggiate() == "scale_down" then
-    position = 1
+    arpeggiate.set_position(1)
     patch.note_off_all()
   else
-    chord_positions = {}
-    last_degree = nil
-    remaining_positions = {}
+    arpeggiate.reset()
     patch.note_off_all()
   end
 end
@@ -361,87 +357,4 @@ function pitch_on_position(i)
   local f = pitches:octdegfreq(params:get("octave"), i)
   pitches_on[f] = i
   pitch_on(f, 120, i)
-end
-
-function arpeggiate.scale_up()
-  position = (position % scale.length) + 1
-  pitches_off()
-  pitch_on_position(position)
-end
-
-function arpeggiate.scale_down()
-  position = position - 1
-  position = position < 1 and scale.length or position
-  pitches_off()
-  pitch_on_position(position)
-end
-
-local chord_positions = {}
-local last_degree = nil
-local remaining_positions = {}
-
-function arpeggiate.chord()
-  if pf.tablelength(chord_positions) == 0 then
-    position = (position % scale.length) + 1
-    -- print("+1", position)
-    remaining_positions = {}
-    local degree = position
-    local matches = chords.match(intervals:interval_labels(degree))
-    local values = {}
-    for n,v in pairs(matches) do
-      values[#values+1] = v
-    end
-    if #values > 0 then
-      local degrees = values[math.random(#values)]
-      chord_positions[degree] = "P1"
-      for deg,int_label in pairs(degrees) do
-        chord_positions[(tonumber(deg) + degree) % scale.length + 1] = int_label
-      end
-    end
-    -- pf.printp(chord_positions)
-    last_degree = (degree % scale.length) + 1
-    -- print('---', last_degree)
-  end
-  if chord_positions[position] ~= nil then
-    chord_positions[position] = nil
-    remaining_positions = {}
-    for i,v in pairs(chord_positions) do
-      if v ~= nil then
-        remaining_positions[#remaining_positions + 1] = i
-      end
-    end
-    pitches_off()
-    pitch_on_position(position)
-    local count = pf.tablelength(chord_positions)
-    position = count > 0 and remaining_positions[1] or math.random(scale.length)
-    -- pf.dprint(position)
-  end
-end
-
-function arpeggiate.chords()
-  position = math.random(scale.length)
-  -- (position % scale.length) + 1
-  if pf.tablelength(chord_positions) == 0 then
-    local degree = position
-    local matches = chords.match(intervals:interval_labels(degree))
-    local values = {}
-    for n,v in pairs(matches) do
-      values[#values+1] = v
-    end
-    if #values > 0 then
-      local degrees = values[math.random(#values)]
-      chord_positions[degree] = "P1"
-      for deg,int_label in pairs(degrees) do
-        chord_positions[(tonumber(deg) + degree) % scale.length + 1] = int_label
-      end
-    end
-  end
-  if chord_positions[position] ~= nil then
-    local f = nil
-    pitches_off()
-    for i,v in pairs(chord_positions) do
-      pitch_on_position(i)
-      chord_positions[i] = nil
-    end
-  end
 end
